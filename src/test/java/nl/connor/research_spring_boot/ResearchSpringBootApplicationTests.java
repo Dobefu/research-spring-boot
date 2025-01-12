fd4573733eb439ee203131e9aabe4d329e3990ea
Connor van Spronssen <info@dobefu.com>
2025-01-12 11:38:53 +0100

    Initial commit

---
+++ src/test/java/nl/connor/research_spring_boot/ResearchSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.connor.research_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
