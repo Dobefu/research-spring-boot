ff6c22e7da5d88f517015bd8f915c5f572cb7e65
Connor van Spronssen <info@dobefu.com>
2025-01-12 12:20:07 +0100

    feat: Add a home route

---
+++ src/main/java/nl/connor/research_spring_boot/HelloController.java
@@ -0,0 +1,14 @@
+package nl.connor.research_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "What's good, world?";
+	}
+
+}
