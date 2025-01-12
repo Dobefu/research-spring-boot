6ab77bb6f60912d1940301237948fb0c6305e2ef
Connor van Spronssen <info@dobefu.com>
2025-01-12 15:39:15 +0100

    feat: Add ThymeLeaf

--- src/main/java/nl/connor/research_spring_boot/Application.java
+++ src/main/java/nl/connor/research_spring_boot/Application.java
@@ -1,12 +1,7 @@
 package nl.connor.research_spring_boot;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/nl/connor/research_spring_boot/IndexController.java
+++ src/main/java/nl/connor/research_spring_boot/IndexController.java
@@ -1,14 +1,14 @@
 package nl.connor.research_spring_boot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class IndexController {
 
 	@GetMapping("/")
 	public String index() {
-		return "What's good, world?";
+		return "index";
 	}
 
 }
